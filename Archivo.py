4802f9696e602a4cdde0b00ba658592c82d2fc38
Yenifer <gemeyeni@hotmail.com>
2018-10-10 18:55:15 -0600

    Interfaz

--- Archivo.py
+++ Archivo.py
@@ -1,21 +1,14 @@
 import easygui as eg
 
 class Archivo:
+#Para cargar el archivo 
 	def cargarAlgoritmo(self,ruta):
 		archivo = open(ruta, "r")
 		linea= archivo.read()
 		return linea
 		
-	
+#Para escoger el archivo que se desea cargar
 	def abrirArchivo(self):
-		extension = ["*.txt","*.txt"]
-		abrir = eg.fileopenbox(msg="Abrir archivo",title="Abrir Algoritmo",default='',filetypes=extension)
+		extension = ['*.txt']
+		abrir = eg.fileopenbox(msg="Abrir archivo",title="Abrir Algoritmo",filetypes=extension)
 		return abrir
-
-		
-		
-		
-		
-		
-		
-		
--- Interfaz.py
+++ Interfaz.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import Archivo
 
-
+#Insertar el algoritmo el la caja de texto
 def insertar():
 	archivo = Archivo.Archivo()
 	ruta =archivo.abrirArchivo()
@@ -12,40 +12,63 @@ def insertar():
 		carga = archivo.cargarAlgoritmo(ruta)
 		text.insert(INSERT,carga)
 		
-		
-		
+#Insertar las hileras de prueba
+def insertarHileras():
+	archivo = Archivo.Archivo()
+	ruta =archivo.abrirArchivo()
+	input = text2.get("1.0",END)
+	if(ruta != None):
+		if(input != ''):
+			text2.delete('1.0', END)
+		carga = archivo.cargarAlgoritmo(ruta)
+		text2.insert(INSERT,carga)
+	    
+
+
 #ventana
 root = Tk()
 root.title('Algoritmos de Markov')
 #tamaño de la ventana
-root.config(width = 900, height = 500)
+root.config(width = 1300, height = 650)
 root.configure(background='SkyBlue2')
 
 #titulo de la ventana
 titulo = Label(root, text = "Algoritmos de Markov ",  font=("Arial", 20,'bold'), background='SkyBlue2')
-titulo.place(x = 310 , y = 10)
-
+titulo.place(x = 510 , y = 10)
 
 #botones
 boton1 = Button(root,text="Abrir Algoritmo" ,command =insertar, font=("Arial", 14),justify =CENTER, relief =RAISED)
-boton1.place(x = 60, y = 60)
+boton1.place(x = 260, y = 100)
 
-boton2 = Button(root,text="Cargar Archivo",  font=("Arial", 14),justify =CENTER, relief =RAISED)
-boton2.place(x = 220, y = 60)
+boton2 = Button(root,text="Cargar Archivo", command=insertarHileras,  font=("Arial", 14),justify =CENTER, relief =RAISED)
+boton2.place(x = 630, y = 100)
 
-boton3 = Button(root,text="Run", font=("Arial", 14),justify =CENTER, relief =RAISED)
-boton3.place(x = 530, y = 60)
+boton3 = Button(root,text="RUN", font=("Arial", 14),justify =CENTER, relief =RAISED)
+boton3.place(x = 780, y = 100)
 
 boton4 = Button(root,text="Guardar Salida", font=("Arial", 14 ), justify =CENTER, relief =RAISED)
-boton4.place(x = 610, y = 60)
-
-#caja de texto
-
-text = Text(root, height=20, width=50, bg = 'gray64')
-text.place(x = 20, y = 110)
+boton4.place(x = 1100, y = 100)
 
 
-text2 = Label(root,height=21, width=57, bg = 'gray64')
-text2.place(x = 450, y = 110)
+#titulos
+label1 = Label(root, text = 'Algoritmo', font = ("Arial", 16 ))
+label1.place(x = 20, y = 110)
 
+label2 = Label( root, text = 'Hileras de Prueba', font = ("Arial", 16 ))
+label2.place(x = 440, y = 110)
+
+label3 = Label( root, text = 'Resultado', font = ("Arial", 16 ))
+label3.place(x = 870, y = 110)
+
+#caja de texto para ubicar el algoritmo
+text = Text(root, height=27, width=50, bg = 'gray64')
+text.place(x = 20, y = 160)
+
+#ubicar las hileras que se le aplicaran las reglas
+text2 = Text( root, height=27, width=50, bg = 'gray64')
+text2.place(x = 440, y = 160)
+
+#ubicar el resultado de las hileras procesadas
+text3 = Text( root, height=27, width=50, bg = 'gray64')
+text3.place(x = 860, y = 160)
 root.mainloop()
