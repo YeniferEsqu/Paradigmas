6c9240b0a006669af6b0095e87c618343155e34b
YeniferEsqu <gemeyeni@hotmail.com>
2018-10-17 20:33:12 -0600

    Algoritmo sin etiquetas

--- Algoritmo.py
+++ Algoritmo.py
@@ -1,4 +1,6 @@
+﻿# -*- coding: utf-8 -*-
 import re
+
 class Algoritmo:
 #variables globales
 	global lista1 #reglas busqueda
@@ -6,26 +8,36 @@ class Algoritmo:
 	global lista3 #simbolos
 	global lista4 #variables
 	global lista5 #marcadores
+	global listadef
 	global variablesUsadas  #lista auxiliar para almacenar las variable que utiliza una regla y su posicion en dicha cadena
 	global etiquetaInicio
+	global hileraSustituida
+	global terminal 
+	global sustitucion
+	global aplico
 	etiquetaInicio = None
 	variablesUsadas=[]
-	lista1=[]
-	lista2=[]
-	lista3=[]
-	lista4=[]
-	lista5=[]
+	
+	lista1=["βX","XXβ","X"]
+	lista2=["XXβ","X.","βX"]
+	lista3=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"]
+	lista4=["X"]
+	lista5=["β"]
+	listadef=[]
+	terminal = False
+	sustitucion=""
+	aplico =False
+	
+
 
 #devuelve posicion de regla terminal	
 	def ReglaTerminal(self):
 		global lista2
 		for x in lista2:
 			if(x.find(".")== 1):
-				terminal = lista2.index(x)
-				return terminal
-
-	
-	
+				terminal2 = x
+				return terminal2
+				
 	def CargarLista(self, algoritmo):
 		global lista1
 		global lista2
@@ -76,12 +88,6 @@ class Algoritmo:
 				simb = list[x].split("->")
 				lista1.append(simb[0])
 				lista2.append(simb[1])
-
-		#print("Lista1", lista1)
-		#print("Lista2", lista2)
-		#print("Lista3", lista3)
-		#print("Lista4", lista4)
-		#print("Lista5", lista5)
 	
 	#metodo para agarrar lo del campo de texto y comprobar que esta en el alfabeto
 	def estaenlalista(self, cadena):
@@ -99,11 +105,11 @@ class Algoritmo:
 		
 		return res
 		
-
+#verifica que las hileras coincidan con los simbolos
 	def evaluar(self,hilera):
 		cadena = hilera
 		lista=[]
-		listadef=[]
+		global listadef
 		num=0
 		#inserta todo lo que tiene el campo de texto de las hileras en la lista
 		for line in cadena.splitlines():			
@@ -116,11 +122,11 @@ class Algoritmo:
 			if (self.estaenlalista(x) == 1):
 				listadef.append(x)
 				num = num + 1
-		print(listadef)
+		return listadef
 
 	#metodo que busca si existe una etiqueta 
 	def buscaEtiqueta(self,regla):
-		etiqueta = re.match("(.+?)+:" , regla)
+		etiqueta = re.match("\w[+?]+:" , regla)
 		if (etiqueta != None):
 			etiqueta = etiqueta.group()
 			etiqueta = re.sub(r":","",etiqueta)
@@ -128,8 +134,8 @@ class Algoritmo:
 		else:
 			return None
 	
-	#metodo que busca si existe una etiqueta al final de una regla 
-	# y la asigna a su correspondiente variable global
+#metodo que busca si existe una etiqueta al final de una regla 
+# y la asigna a su correspondiente variable global
 	def buscaEtiquetaInicio(self,reglaSustitucion):
 		global etiquetaInicio
 		etiqueta = re.search("([(].+?[)])$" , reglaSustitucion)
@@ -142,13 +148,9 @@ class Algoritmo:
 		else:
 			etiquetaInicio = None
 			return None
-	
-	
-	#metodo que convierte una regla de busqueda en un patron de busqueda de expresiones regulares
+
+#metodo que convierte una regla de busqueda en un patron de busqueda de expresiones regulares
 	def convertirPatron(self,etiqueta,regla):
-		global lista3
-		global lista4
-		global lista5
 		global variablesUsadas
 		variablesUsadas =[]
 		if (etiqueta != None):
@@ -176,7 +178,7 @@ class Algoritmo:
 				if cont == 0:
 					patron = patron +"."
 				else:
-					patron = patron +"."
+					patron = patron +"+."
 				cont = cont + 1
 			elif regla[x] in lista5:   #si esta en la lista de marcadores
 				if cont == 0:
@@ -188,7 +190,7 @@ class Algoritmo:
 				return None
 		return patron
 
-	#recibe un patron y una hilera y determina si existe alguna coincidencia
+#recibe un patron y una hilera y determina si existe alguna coincidencia
 	def buscarPatrones(self,patron,hilera):
 		global variablesUsadas
 		if patron == "^":
@@ -221,12 +223,14 @@ class Algoritmo:
 						else:
 							if variablesUsadas[i + 2] == variablesUsadas[j + 2]:
 								return None
+
+
 			return patronEncontrado
 		else:
 			return None
 
-	#recibe el patron encontrado en el metodo anterior, la regla de sustitucion correspondiente 
-	#y la hilera a la que se va a aplicar. Devuelve la hilera con la sustitucion hecha
+#recibe el patron encontrado en el metodo anterior, la regla de sustitucion correspondiente 
+#y la hilera a la que se va a aplicar. Devuelve la hilera con la sustitucion hecha
 	def sustituirRegla(self,patronEncontrado,reglaSustitucion,hilera):
 		global variablesUsadas
 		global lista3
@@ -252,14 +256,91 @@ class Algoritmo:
 					bandera = False
 					break
 			if bandera:
-				if reglaSustitucion[i] in lista3 or reglaSustitucion[i] in lista4  or reglaSustitucion[i] in lista5:
-					sustitucion = sustitucion + reglaSustitucion[i]
+				if reglaSustitucion[i] in lista3 or reglaSustitucion[i] in lista4  or reglaSustitucion[i] in lista5 or reglaSustitucion[i] == '.':
+						if reglaSustitucion[i] != '.':
+							sustitucion = sustitucion + reglaSustitucion[i]
 				else:
 					return None
 		
 		return re.sub(patronEncontrado,sustitucion,hilera,1)
 
+#aplica las reglas 1 vez 
+	def SustituirCadena(self,hilera):
+		tam = len(lista1)
+		global aplico
+		restart = True
+		global terminal 
+		global sustitucion
+		for i in range(0,tam):
+			#print("Regla: ")
+			#print(lista1[i])
+			etiqueta = self.buscaEtiqueta(lista1[i])
+			#print("Etiqueta: ")
+			#print(etiqueta)
+			patron = self.convertirPatron(etiqueta,lista1[i])
+			#print("Patron: ")
+			#print(patron)
+			patronEncontrado = self.buscarPatrones(patron,hilera)
+			#print("Patron Encontrado: ")
+			#print(patronEncontrado)
+			if patronEncontrado != None:
+				print("Regla Sustitucion: ")
+				print(lista2[i])
+				print("Hilera Resultante: ")
+				sustitucion = self.sustituirRegla(patronEncontrado,lista2[i],hilera)
+				#print(sustitucion)
+				print("ReglaTerminal")
+				print(self.ReglaTerminal())
+				aplico = True
+				if(self.ReglaTerminal() == lista2[i]):
+					terminal = True
+				break
+		print("sustitucion  "+sustitucion)
+		return sustitucion
+		
+				
+#agarra lo que tiene sustitucion y sigue aplicando regla si no aplica regla terminal
+	def recorrerHilera(self,hilera):
+		global terminal 
+		sustitucion = self.SustituirCadena(hilera)
+#si no aplico regla terminal que siga evaluando
+		while(terminal is False or aplico is False):
+			sustitucion =self.SustituirCadena(sustitucion)
+			
 
+#la primera vez que entra cambia la hilera, luego agarra lo que tiene la variable global sustitucion y lo evalua para aplicar las reglas 
+	def CambiaHilera(self, hilera):
+		global sustitucion		
+		if(sustitucion==""):
+			sustitucion =self.recorrerHilera(hilera)
+		else:
+			sustitucion =self.recorrerHilera(sustitucion)
+
+#metodo que recorre la listaDefinidos y la evalua para aplicar las reglas
+	def ListaaEvaluar(self,hilera):
+		listaDefinidos = []
+		global terminal 
+		global sustitucion
+		global aplico
+		listaDefinidos = self.evaluar(hilera)
+		for i in listaDefinidos:
+			self.CambiaHilera(i)
+			#print(listaDefinidos)
+			print("\n")
+			terminal = False
+			sustitucion=""
+			aplico =False
+		
+a = Algoritmo()
+hilera = """abc
+nop
+asd
+cvb"""
+
+a.ListaaEvaluar(hilera)
+
+
+	
 
 
 
